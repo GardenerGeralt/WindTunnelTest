eaa42935e945fcdbd061b1912e9c61e420261987
GardenerGeralt <m.maximchuk@student.tudelft.nl>
2021-12-03 13:09:28 +0100

    .

--- 2D_plots.py
+++ 2D_plots.py
@@ -1,1 +1,18 @@
 from plotly import express as px
+import pandas as pd
+
+
+def liftpolar(filename):
+    df = pd.read_csv(filename)
+    fig = px.line(df, x='α / °', y='c_l', title='Lift Polar')
+
+
+def dragpolar(filename):
+    df = pd.read_csv(filename)
+    fig = px.line(df, x='c_l', y='c_d', title='Drag Polar')
+
+
+def momentpolar(filename):
+    df = pd.read_csv(filename)
+    fig = px.line(df, x='α / °', y='c_m', title='Pitching Moment Polar')
+
