d42c0a18a39def5b74511a45adb8b57c24eacd0e
acpetrache <andrei.c.petrache@gmail.com>
2021-12-23 15:33:26 +0100

    Updated the code, made folders for the save files

--- Group29/3D polars/experiment_polars.py
+++ Group29/3D polars/experiment_polars.py
@@ -19,15 +19,6 @@ data = data.astype(float)  # turning the strings into floats
 # print(data)
 
 
-fig1, ax1 = plt.subplots(figsize=(8,5))
-ax1.plot(xlst, slopelst, linewidth=1, marker='o', markersize=2.5, label='Cl slope')
-# ax1.set_title('$C_L-\alpha$')
-ax1.set_xlabel(r'$\alpha$ [deg]')
-ax1.set_ylabel(r'${C_L}_\alpha$ [-]')
-plt.grid()
-plt.show()
-
-
 
 print("Avaliable polars:\n 1. All\n 2.CL-alpha\n 3.CD-alpha\n 4.CMpitch-alpha\n 5.CMroll-alpha\n 6.CMyaw-alpha\n"
       " 7.CL-CD")
@@ -42,10 +33,10 @@ if choice==2:
     # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2)  # only for visualising all 4 at once
     # fig.set_size_inches(8, 5)
     fig1, ax1 = plt.subplots(figsize=(8,5))
-    ax1.plot(data[:,1], data[:,3], linewidth=1, marker='o', markersize=2.5, label='Cl-alpha')
+    ax1.plot(data[:,1], data[:,3], linewidth=1, marker='o', markersize=4, label='Cl-alpha')
     # ax1.set_title('$C_L-\alpha$')
-    ax1.set_xlabel(r'$\alpha$ [deg]')
-    ax1.set_ylabel(r'$C_L$ [-]')
+    ax1.set_xlabel(r'$\alpha$ [deg]', fontsize=16)
+    ax1.set_ylabel(r'$C_L$ [-]', fontsize=16)
     plt.grid()
     plt.savefig('CL_alpha_3dtest.pdf', dpi=200)
     if all:
@@ -53,10 +44,10 @@ if choice==2:
 
 if choice==3:
     fig2, ax2 = plt.subplots(figsize=(8,5))
-    ax2.plot(data[:,1], data[:,4], linewidth=1, marker='^', markersize=2, color='purple', label='Cd-alpha')
+    ax2.plot(data[:,1], data[:,4], linewidth=1, marker='^', markersize=4, color='purple', label='Cd-alpha')
     # ax2.set_title('Cd-alpha')
-    ax2.set_xlabel(r'$\alpha$ [deg]')
-    ax2.set_ylabel(r'$C_D$ [-]')
+    ax2.set_xlabel(r'$\alpha$ [deg]', fontsize=16)
+    ax2.set_ylabel(r'$C_D$ [-]', fontsize=16)
     plt.grid()
     plt.savefig('CD_alpha_3dtest.pdf', dpi=200)
     if all:
@@ -64,10 +55,10 @@ if choice==3:
 
 if choice==4:
     fig3, ax3 = plt.subplots(figsize=(8,5))
-    ax3.plot(data[:,1], data[:,11], linewidth=1, marker='s', markersize=2, color='orange', label='Cm-alpha')
+    ax3.plot(data[:,1], data[:,11], linewidth=1, marker='s', markersize=4, color='orange', label='Cm-alpha')
     # ax3.set_title('Cm_pitch-alpha')
-    ax3.set_xlabel(r'$\alpha$ [deg]')
-    ax3.set_ylabel(r'$(C_M)_{pitch}$ [-]')
+    ax3.set_xlabel(r'$\alpha$ [deg]', fontsize=16)
+    ax3.set_ylabel(r'$(C_M)_{pitch}$ [-]', fontsize=16)
     plt.grid()
     plt.savefig('CMpitch_alpha_3dtest.pdf', dpi=200)
     if all:
@@ -75,9 +66,9 @@ if choice==4:
 
 if choice==5:
     fig4, ax4 = plt.subplots(figsize=(8,5))
-    ax4.plot(data[:,1], data[:,10], linewidth=1, marker='s', markersize=2, color='orange', label='Cm-alpha')
-    ax4.set_xlabel(r'$\alpha$ [deg]')
-    ax4.set_ylabel(r'$(C_M)_{roll}$ [-]')
+    ax4.plot(data[:,1], data[:,10], linewidth=1, marker='s', markersize=4, color='orange', label='Cm-alpha')
+    ax4.set_xlabel(r'$\alpha$ [deg]', fontsize=16)
+    ax4.set_ylabel(r'$(C_M)_{roll}$ [-]', fontsize=16)
     plt.grid()
     plt.savefig('CMroll_alpha_3dtest.pdf', dpi=200)
     if all:
@@ -85,9 +76,9 @@ if choice==5:
 
 if choice==6:
     fig5, ax5 = plt.subplots(figsize=(8,5))
-    ax5.plot(data[:,1], data[:,12], linewidth=1, marker='s', markersize=2, color='orange', label='Cm-alpha')
-    ax5.set_xlabel(r'$\alpha$ [deg]')
-    ax5.set_ylabel(r'$(C_M)_{yaw}$ [-]')
+    ax5.plot(data[:,1], data[:,12], linewidth=1, marker='s', markersize=4, color='orange', label='Cm-alpha')
+    ax5.set_xlabel(r'$\alpha$ [deg]', fontsize=16)
+    ax5.set_ylabel(r'$(C_M)_{yaw}$ [-]', fontsize=16)
     plt.grid()
     plt.savefig('CMyaw_alpha_3dtest.pdf', dpi=200)
     if all:
@@ -95,10 +86,10 @@ if choice==6:
 
 if choice==7:
     fig6, ax6 = plt.subplots(figsize=(8,5))
-    ax6.plot(data[:,4], data[:,3], linewidth=1, marker='D', markersize=2, color='red', label='Cl-Cd')
+    ax6.plot(data[:,4], data[:,3], linewidth=1, marker='D', markersize=4, color='red', label='Cl-Cd')
     # ax4.set_title('Cl-Cd')
-    ax6.set_xlabel(r'$C_D$ [-]')
-    ax6.set_ylabel(r'$C_L$ [-]')
+    ax6.set_xlabel(r'$C_D$ [-]', fontsize=16)
+    ax6.set_ylabel(r'$C_L$ [-]', fontsize=16)
     plt.grid()
     plt.savefig('CL_CD_3dtest.pdf', dpi=200)
 
